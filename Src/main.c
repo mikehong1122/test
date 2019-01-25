7980b57de5673626b419378d5c0153040f866003
mikehong1122 <20455077@qq.com>
2019-01-25 11:48:16 +0800

    my first test git prj

---
+++ Src/main.c
@@ -0,0 +1,99 @@
+/**
+
+  ******************************************************************************
+  */
+/* 包含头文件 ----------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+#include "usart/bsp_debug_usart.h"
+#include "DS18B20/bsp_DS18B20.h"
+
+/* 私有类型定义 --------------------------------------------------------------*/
+/* 私有宏定义 ----------------------------------------------------------------*/
+/* 私有变量 ------------------------------------------------------------------*/
+/* 扩展变量 ------------------------------------------------------------------*/
+/* 私有函数原形 --------------------------------------------------------------*/
+/* 函数体 --------------------------------------------------------------------*/
+/**
+  * 函数功能: 系统时钟配置
+  * 输入参数: 无
+  * 返 回 值: 无
+  * 说    明: 无
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;  // 外部晶振，8MHz
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;  // 9倍频，得到72MHz主时钟
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;       // 系统时钟：72MHz
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;              // AHB时钟：72MHz
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;               // APB1时钟：36MHz
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;               // APB2时钟：72MHz
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+
+ 	// HAL_RCC_GetHCLKFreq()/1000    1ms中断一次
+	// HAL_RCC_GetHCLKFreq()/100000	 10us中断一次
+	// HAL_RCC_GetHCLKFreq()/1000000 1us中断一次
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);  // 配置并启动系统滴答定时器
+  /* 系统滴答定时器时钟源 */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+  /* 系统滴答定时器中断优先级配置 */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/**
+  * 函数功能: 主函数.
+  * 输入参数: 无
+  * 返 回 值: 无
+  * 说    明: 无
+  */
+int main(void)
+{ 
+  uint8_t i, DS18B20ID[8];
+  float temperature;
+  
+  /* 复位所有外设，初始化Flash接口和系统滴答定时器 */
+  HAL_Init();
+  /* 配置系统时钟 */
+  SystemClock_Config();
+
+  /* 初始化串口并配置串口中断优先级 */
+  MX_DEBUG_USART_Init();
+  
+  printf("DS18B20温度传感器信息读取\n");
+  while(DS18B20_Init())	
+  {
+		printf("DS18B20温度传感器不存在\n");    
+    HAL_Delay(1000);
+  }
+  printf("检测到DS18B20温度传感器，并初始化成功\n");
+  DS18B20_ReadId(DS18B20ID);
+  printf("DS18B20的序列号是： 0x");  
+	for ( i = 0; i < 8; i ++ )             
+	  printf ( "%.2X", DS18B20ID[i]);
+  printf("\n");  
+  
+  DS18B20_GetTemp_MatchRom(DS18B20ID);
+  HAL_Delay(100);
+  
+  /* 无限循环 */
+  while (1)
+  {
+    temperature=DS18B20_GetTemp_SkipRom();
+    /* 打印通过 DS18B20 序列号获取的温度值 */
+    printf("获取该序列号器件的温度：%.1f\n",temperature);
+    /* 1s 读取一次温度值 */
+    HAL_Delay(1000);
+  }
+}
+
+/******************* (C) COPYRIGHT 2015-2020 硬石嵌入式开发团队 *****END OF FILE****/
